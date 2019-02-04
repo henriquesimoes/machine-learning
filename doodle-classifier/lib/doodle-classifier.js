769f038f17dab1de28b7f46a2db67c213220a61c
Henrique <henriquesimoespa@gmail.com>
2019-02-04 14:44:20 -0200

    Refactor code for doodle-classifier

---
+++ doodle-classifier/lib/doodle-classifier.js
@@ -0,0 +1,113 @@
+'use strict';
+class DoodleClassifier {
+  constructor (total = 1000) {
+    this.cats = {
+      code: 0,
+      label: "cat",
+      source: `cats${total}.bin`,
+      training: [],
+      testing: []
+    };
+    this.trains = {
+      code: 1,
+      label: "train",
+      source: `trains${total}.bin`,
+      training: [],
+      testing: []
+    };
+    this.rainbows = {
+      code: 2,
+      label: "rainbow",
+      source: `rainbows${total}.bin`,
+      training: [],
+      testing: []
+    };
+
+    this.cats.length = this.trains.length = this.rainbows.length = total;
+    this.trainingPercentage = 0.8; // 80% of the data for training and 20% for testing
+    this.length = 784; // Bytes length for a single image (28x28)
+    this.training = [];
+    this.testing = [];
+    this.trainingCounter = 0;
+    this.testingCounter = 0;
+    this.correctAnswers = 0;
+    this.x = 0;
+    
+    this.nn = new NeuralNetwork(784, 64, 3);  
+  }
+
+  loadData () {    
+    this.cats.data = loadBytes(`data/${this.cats.source}`);
+    this.trains.data = loadBytes(`data/${this.trains.source}`);
+    this.rainbows.data = loadBytes(`data/${this.rainbows.source}`);
+  }
+
+  prepareData () {
+    [this.cats, this.trains, this.rainbows].forEach((category) => this.prepareCategory(category));
+    this.training = [].concat(this.cats.training, this.trains.training, this.rainbows.training);
+    shuffle(this.training, true);
+    this.testing = [].concat(this.cats.testing, this.trains.testing, this.rainbows.testing);
+  }
+
+  prepareCategory (category) {    
+    for (let i=0; i< category.length; i++) {
+      let offset = this.length * i;
+      if (i < Math.floor(category.length * this.trainingPercentage)) {
+        category.training.push({label: category.code, data: category.data.bytes.subarray(offset, offset + this.length)});
+      } else {
+        category.testing.push({label: category.code, data: category.data.bytes.subarray(offset, offset + this.length)});
+      }
+    }
+  }
+
+  trainEpoch () {
+    shuffle(this.training, true);
+    for (let i = 0; i < this.training.length; i++) {
+      this.trainNext(i);
+    }
+  }
+
+  trainNext(i) {
+    let raw = Array.from(this.training[i].data);
+    let inputs = raw.map(v => v / 255.0);
+    let targets = new Array(this.nn.nOutputNodes).fill(0);
+    targets[this.training[i].label] = 1;
+    this.nn.train(inputs, targets);
+    this.trainingCounter++;
+  }
+
+  test () {
+    for (let i=0; i < this.testing.length; i++) {
+      this.testNext(i);
+    }
+    console.log(this.correctAnswers / this.testing.length);
+  }
+
+  testNext(i) {
+    let guess = this.nn
+      .predict(Array.from(this.testing[i].data));
+    let classification = guess.indexOf(max(guess));
+    if (classification === this.testing[i].label) this.correctAnswers++;
+    this.testingCounter++;
+  }
+
+  get trainingLength () {
+    return this.training.length;
+  }
+
+  get testingLength () {
+    return this.testing.length;
+  }
+
+  get trainingProgress () {
+    return Math.round(this.trainingCounter / this.training.length * 100);
+  }
+
+  get testingProgress () {
+    return Math.round(this.testingCounter / this.testing.length * 100);
+  }
+
+  get accuracy () {
+    return Math.round(this.correctAnswers / this.testingCounter * 100);
+  }
+}
--- doodle-classifier/sketch.js
+++ doodle-classifier/sketch.js
@@ -1,116 +1,31 @@
-let nn;
-const total = 5000;
-let cats = {
-	code: 0,
-	label: "cat",
-	source: `cats${total}.bin`,
-	training: [],
-	testing: []
-};
-let trains = {
-	code: 1,
-	label: "train",
-	source: `trains${total}.bin`,
-	training: [],
-	testing: []
-};
-let rainbows = {
-	code: 2,
-	label: "rainbow",
-	source: `rainbows${total}.bin`,
-	training: [],
-	testing: []
-};
-
-const trainingPercentage = 0.8; // 80% of the data for training and 20% for testing
-const length = 784; // Bytes length for a single image (28x28)
-let trainingProgress = 0;
-let testingProgress = 0;
-let correctAnswers = 0;
-let accuracy = 0;
-let training;
-let testing;
-let counter = 0;
+let ddc;
 
 function preload() {
-	cats.data = loadBytes(`data/${cats.source}`);
-	trains.data = loadBytes(`data/${trains.source}`);
-	rainbows.data = loadBytes(`data/${rainbows.source}`);
-	cats.length = trains.length = rainbows.length = total;
+	ddc = new DoodleClassifier();
+	ddc.loadData();
 }
 
 function setup() {
 	createCanvas(280, 280);
-	[cats, trains, rainbows].forEach((category) => prepareData(category));
 	frameRate(100);
-	nn = new NeuralNetwork(784, 64, 3);
-
-	training = [].concat(cats.training, trains.training, rainbows.training);
-	shuffle(training, true);
-	// trainEpoch(training);
-	
-	testing = [].concat(cats.testing, trains.testing, rainbows.testing);
-	// test(testing);
+	ddc.prepareData();
 }
 
 function draw() {
+	let counter = frameCount - 1;
 	background(0);
 	textSize(16);
 	textAlign(CENTER, CENTER)
 	fill(255);
 
-	let testingCoutner = ++counter - training.length;
-	if (counter < training.length) {
-		trainNext(training, counter);
-	} else if (testingCoutner < testing.length) {
-		testNext(testing, testingCoutner);
-		accuracy = Math.round(correctAnswers / testingCoutner * 100);
+	let testingCounter = counter - ddc.trainingLength;
+	if (counter < ddc.trainingLength) {
+		ddc.trainNext(counter);
+	} else if (testingCounter < ddc.testingLength) {
+		ddc.testNext(testingCounter);
 	}
 
-	text(`Tranining progress: ${trainingProgress}%`, width / 2, height / 4);
-	text(`Testing progress: ${testingProgress}%`, width / 2, height / 2);
-	text(`Accuracy: ${accuracy}%`, width / 2, height * 3 / 4);
-}
-
-function prepareData (category) {
-	for (let i=0; i< category.length; i++) {
-		let offset = length * i;
-		if (i < Math.floor(category.length * trainingPercentage)) {
-			category.training.push({label: category.code, data: category.data.bytes.subarray(offset, offset + length)});
-		} else {
-			category.testing.push({label: category.code, data: category.data.bytes.subarray(offset, offset + length)});
-		}
-	}
-}
-
-function trainEpoch (training) {
-	shuffle(training, true);
-	for (let i = 0; i < training.length; i++) {
-		trainNext(training, i);
-	}
-}
-
-function trainNext(training, i) {
-	let raw = Array.from(training[i].data);
-	let inputs = raw.map(v => v / 255.0);
-	let targets = new Array(nn.nOutputNodes).fill(0);
-	targets[training[i].label] = 1;
-	nn.train(inputs, targets);
-
-	trainingProgress = Math.round(i / training.length * 100);
-}
-
-function test (testing) {
-	for (let i=0; i < testing.length; i++) {
-		testNext(testing, i);
-	}
-	console.log(correctAnswers / testing.length);
-}
-
-function testNext(testing, i) {
-	let guess = nn
-		.predict(Array.from(testing[i].data));
-	let classification = guess.indexOf(max(guess));
-	if (classification === testing[i].label) correctAnswers++;
-	testingProgress = Math.round(i / testing.length * 100);
+	text(`Tranining progress: ${ddc.trainingProgress}%`, width / 2, height / 4);
+	text(`Testing progress: ${ddc.testingProgress}%`, width / 2, height / 2);
+	text(`Accuracy: ${ddc.accuracy}%`, width / 2, height * 3 / 4);
 }
